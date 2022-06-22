72fcdc42b63172b04008ae976f041a728185b3a7
Manuel Comezaña <macq09876@gmail.com>
2022-06-22 16:20:21 -0500

    Mostrar imágenes de las películas

--- lib/widgets/card_swiper.dart
+++ lib/widgets/card_swiper.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_card_swipper/flutter_card_swiper.dart';
 
-import 'package:card_swiper/card_swiper.dart';
+//import 'package:card_swiper/card_swiper.dart';
 import 'package:peliculas/models/models.dart';
 
 class CardSwiper extends StatelessWidget {
@@ -17,27 +18,30 @@ class CardSwiper extends StatelessWidget {
     return Container(
       width: double.infinity,
       height: size.height * 0.5,
-      child: Swiper(
-        itemCount: 10,
+      child: movies.length > 2 ? Swiper(
+        itemCount: movies.length,
         layout: SwiperLayout.STACK,
         itemWidth: size.width * 0.6,
         itemHeight: size.height * 0.4,
-        itemBuilder: (BuildContext context, int index) {
+        itemBuilder: ( _ , int index) {
+
+          final movie = movies[index];
+          print(movie.posterPath);
           
           return GestureDetector(
             onTap: () => Navigator.pushNamed(context, 'details', arguments: 'movie-instance'),
             child: ClipRRect(
               borderRadius: BorderRadius.circular(20),
-              child: const FadeInImage(
+              child: FadeInImage(
                 placeholder: AssetImage('assets/no-image.jpg'),
-                image: NetworkImage('https://via.placeholder.com/300x400'),
+                image: NetworkImage(movie.fullPosterImg),
                 fit: BoxFit.cover,
               ),
             ),
           );
 
         },
-      ),
+      ) : null,
     );
   }
 }
